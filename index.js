aefa6900d4b422e4e18111e859926e0908a23747
William Damm <wdamm@powerhrg.com>
2020-10-31 22:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing (katzDeliLine) {
 
 
 function currentLine (katzDeliLine) {
-  arr = [];
+  let arr = [];
  if (katzDeliLine.length === 0) {
    return "The line is currently empty.";
  }
